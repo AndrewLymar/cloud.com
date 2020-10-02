d7d7b5b086abdc86d84f7768b3515baa14072a94
andrewlymar <happyandrual@gmail.com>
2020-10-02 20:28:32 +0300

    Up

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,17 @@
 $(function () {
     $(".question__inner").css("display", "none");
+    
     $(".question__outer").click(function () {
         $(this).siblings().slideToggle();
+        let step = 180;
+        let el = $(this).find(".question__img");
+        let d = el.data("angle") + step;
+        el.css("transform", "rotate(+" + d + "deg)");
+        if (d >= 360) d -= 360;
+        el.data("angle", d);
     });
 
-    $("nav").mobileMenu({
+    $(".header__menu").mobileMenu({
         menuIconClassName: ".menu__icon",
         mobileResolution: 768,
         menuType: "sticky",
@@ -12,7 +19,23 @@ $(function () {
         closeIconClassName: ".close-menu-icon"
     });
 
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.swiper-hero', {
+        pagination: {
+            el: '.swiper-pagination'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        loop: {
+            loop: 'true'
+        },
+        autoplay: {
+            delay: 4000,
+        },
+    });
+    
+    var swiper = new Swiper('.swiper-reviews', {
         pagination: {
             el: '.swiper-pagination'
         },
